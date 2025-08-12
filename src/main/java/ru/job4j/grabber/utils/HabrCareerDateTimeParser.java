f39f5896ebec0f77d624da2d4d36d2c808fdeed2
margarita <margarita.que@mail.ru>
2025-08-12 23:57:18 +0400

    2.1. Преобразование даты [#289476]

--- src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
+++ src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
@@ -3,7 +3,6 @@ package ru.job4j.grabber.utils;
 import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
 
-
 public class HabrCareerDateTimeParser implements DateTimeParser {
     @Override
     public LocalDateTime parse(String parse) {
